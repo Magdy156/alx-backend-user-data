2e67acc290c9699b923ae6b2726bc44962b977ae
Magdy156 <magdyhamdy156@gmail.com>
2024-08-13 08:09:55 +0300

    task0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Filter module"""
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str, message: str, separator: str) -> str:
+    """Fileter func"""
+    for field in fields:
+        message = re.sub(
+            f'{field}=(.*?){separator}',
+            f'{field}={redaction}{separator}', message)
+    return message
