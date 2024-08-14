0d27965dcf722abe5755e8a4ae4355866b82c19d
Magdy156 <magdyhamdy156@gmail.com>
2024-08-15 00:13:54 +0300

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ encrypt the password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ compare 2 passwords"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
