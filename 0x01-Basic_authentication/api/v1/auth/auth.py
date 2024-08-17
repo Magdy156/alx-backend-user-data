2aff0d134255defb4ad1813da6603b3538e861a4
Magdy156 <magdyhamdy156@gmail.com>
2024-08-17 08:40:39 +0300

    task3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ API Authentication Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """will be defined later"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ handles authorization header """
+        return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
+        return None
