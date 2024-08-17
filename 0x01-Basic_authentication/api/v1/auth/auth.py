43fe00c9c5ee12f18f8715ac6bcf6ef81b8d95d3
Magdy156 <magdyhamdy156@gmail.com>
2024-08-17 09:22:01 +0300

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,37 @@ class Auth:
     """ API Authentication Class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """will be defined later"""
-        return False
+        """ returns True if the path is not in
+        the list of strings excluded_paths
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        pathLength = len(path)
+        if pathLength == 0:
+            return True
+
+        if path[pathLength - 1] == '/':
+            slash_path = True
+        else:
+            slash_path = False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for pathe in excluded_paths:
+            pathLength = len(pathe)
+            if pathLength == 0:
+                continue
+
+            if pathe[pathLength - 1] != '*':
+                if tmp_path == pathe:
+                    return False
+            else:
+                if pathe[:-1] == pathe[:pathLength - 1]:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ handles authorization header """
