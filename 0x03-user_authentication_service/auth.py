b4aaec4aa1ef20e39ce18c200bbc523bfedc9bd3
Magdy156 <magdyhamdy156@gmail.com>
2024-08-19 08:50:37 +0300

    auth_module

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Module for authentication.
+"""
+import logging
+import bcrypt
+from uuid import uuid4
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+logging.disable(logging.WARNING)
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user with the given email and password.
+        """
+        try:
+            # find user
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's email and password are valid.
+        """
+        try:
+            # find user
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                encodedPassword = password.encode('utf-8')
+                hashed_password = user.hashed_password
+                if bcrypt.checkpw(encodedPassword, hashed_password):
+                    return True
+        except NoResultFound:
+            return False
+        return False
