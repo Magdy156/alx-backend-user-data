f31ac2a19341cba44824ff62fa5f23509da296d0
Magdy156 <magdyhamdy156@gmail.com>
2024-08-19 04:40:59 +0300

    task1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """User Model"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-Base = declarative_base
+Base = declarative_base()
 
 
 class User(Base):
