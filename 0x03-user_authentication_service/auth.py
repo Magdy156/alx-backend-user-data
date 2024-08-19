ce0ee39197eb0a430c375d4abc70f96f135dbe23
Magdy156 <magdyhamdy156@gmail.com>
2024-08-19 12:40:31 +0300

    13ubdate

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,8 +78,6 @@ class Auth:
             return None
         return user
 
-    def estroy_session(self, user_id: int) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """updates the corresponding user’s session ID to None"""
-        if user_id is None:
-            return None
         self._db.update_user(user_id, session_id=None)
