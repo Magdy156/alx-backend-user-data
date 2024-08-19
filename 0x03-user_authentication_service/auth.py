b133426ba44728e4a558d392957eee7631192881
Magdy156 <magdyhamdy156@gmail.com>
2024-08-19 08:51:43 +0300

    auth_module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,6 +15,7 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
